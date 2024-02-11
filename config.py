5228a517ed2e37865c7291167e0b25ac6e0b6c4a
Serhii Moiseiev <s.moiseiev94@gmail.com>
2024-02-11 11:02:25 +0200

    Add .gitignore and config file

---
+++ config.py
@@ -0,0 +1,13 @@
+"""
+ Config info
+"""
+token = '6947459220:AAFRdOgQ_V-q2upBjQPA5B2WWNkjke-HS00'
+dictName = "dictShelve"
+examplesDictName = "examplesShelve"
+
+url = "https://twinword-word-graph-dictionary.p.rapidapi.com/example/"
+
+headers = {
+	"X-RapidAPI-Key": "e78045a7c7mshbb040f978ce9ebep1e90d4jsne564ba2da058",
+	"X-RapidAPI-Host": "twinword-word-graph-dictionary.p.rapidapi.com"
+}
